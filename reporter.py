7aaae62c2c45577f30d0ab133355b1a30a08412a
EC2 Default User <ec2-user@ip-172-31-4-225.ap-south-1.compute.internal>
2020-06-06 18:49:44 +0000

    added more functions

--- reporter.py
+++ reporter.py
@@ -1,39 +1,63 @@
-# !/usr/bin/env python
+# !/usr/bin/python3
 
-import sys, subprocess, json, os
+import sys, subprocess, json, os 
 import requests
+from datetime import datetime
 import socket
 
 
 def convert_to_json(res):
-    return json.dump(res)
+    return json.dumps(res)
 
-def disk_usage_mem():
-    disklog = []
-    df = subprocess.Popen(["df| grep -w / "], shell=True ,stdout=subprocess.PIPE)
-    for line in df.stdout:
-        splitline = line.decode().split()
-        disklog.append(splitline)
-    data = [disklog]
-    return(data)
+def disk_usage_mem(interval = None):
+    df = subprocess.Popen(["df -h| grep -w / "], shell=True ,stdout=subprocess.PIPE)
+    out,err = df.communicate()
+    payload= {'hostname':socket.gethostname(),'mountPoint':disklog[-1],'totalsize':disklog[1],'used':disklog[2],
+            'avail':disklog[3],'percentageused':disklog[4]}
+    payload['time']= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    disklog = out.decode().split()
+    # if interval:
+    #     threading.Timer(interval,disk_usage_mem).start()
+    print(payload)
+    return convert_to_json(payload)
 
 def get_metadata():
     url='http://169.254.169.254/latest/meta-data/'
-    fileds=['instance-id' ,'instance-type' , 'public-ipv4', 'ami-id', 'hostname']
+    fields=['instance-id' ,'instance-type' , 'public-ipv4', 'ami-id', 'hostname']
     res={}
     for field in fields:
         req= requests.get(url+field)
-        res[field]=req.text()
+        res[field]=req.content.decode()
     return json.dumps(res)
+
+def get_mem_usage():
+    mem = subprocess.Popen(["free -h| grep Mem "], shell=True ,stdout=subprocess.PIPE)
+    out,err = mem.communicate()
+    mem = out.decode().split()
+    MemUtilization_per = int(((int(mem[1][:-1])-int(mem[-1][:-1]))/int(mem[1][:-1]))*100)
+    payload = {'Total_Mem':mem[1],'MemoryUtilized':str(MemUtilization_per)+' %',
+                'Available_Mem':mem[-1],'Mem_used':mem[2]}
+    payload['time']= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    return convert_to_json(payload)
+
+def CPUUtilization():
+    cpu = subprocess.Popen(["sar| grep Average "], shell=True ,stdout=subprocess.PIPE)
+    out,err = cpu.communicate()
+    cpu = out.decode().split()
+    cpu_usage = 100 - float(cpu[-1])
+    payload ={'CPUUtilization': str(round(cpu_usage,2)) + ' %'}
+    payload['time']= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    return convert_to_json(payload)
+
+
+
 
 if __name__ == "__main__":
     path = '/'
     url = 'https://diskbot.cloudstuff.tech/post'
-    log = disk_usage_mem()
-    for disklog in log[0]:
-        if path in disklog[5] and len(disklog[-1]) == 1:
-            payload= {'hostname':socket.gethostname(),'mountPoint':disklog[-1],'totalsize':disklog[1],'used':disklog[2],
-            'avail':disklog[3],'percentageused':disklog[4]}
+    log = CPUUtilization()
             # r = requests.post(url, data=json.dumps(payload),headers={"Content-Type": "application/json"})#provide with url
             # print(r.text)
-            print(payload)
+    print(log)
