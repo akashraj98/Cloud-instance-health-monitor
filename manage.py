c0af5b0e565fb3b0032aab0a1588ed9fffe953c1
Akashraj98 <akashraj7713@gmail.com>
2020-06-09 20:35:19 +0000

    minor changes

--- manage.py
+++ manage.py
@@ -3,50 +3,58 @@
 from flask import Flask, request ,jsonify
 import sqlite3
 
-api =Flask(__name__)
+managerapp =Flask(__name__)
 
-conn = sqlite3.connect('instance.db)
+conn = sqlite3.connect("sqlite3.db")
 
 cursor= conn.cursor()
-table = 'Instance'
+table = 'Report'
 
 #Table is created
-cursor.execute('CREATE TABLE IF NOT EXISTS {table} (INSTANCE_ID TEXT ,HOSTNAME TEXT,INSTANCE_TYPE TEXT,\
+cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} (INSTANCE_ID TEXT ,HOSTNAME TEXT,INSTANCE_TYPE TEXT,\
                 PUBLIC_IPV4 TEXT,CPUUTILIZATION INTEGER,DISKUTILIZATION INTEGER,DISKSIZE TEXT \
                 DISKAVAIL TEXT,DISKUSED TEXT,MEMORYUSAGE TEXT , MEMORYUTILIZATION INTEGER \
-                MEMORYTOTAL TEXT, MEMORYAVAIL TEXT, LOG_TIME TEXT );'.format{table})
+                MEMORYTOTAL TEXT, MEMORYAVAIL TEXT, LOG_TIME TEXT );")
 
 conn.commit()
 conn.close()
 
-@app.route('app/post/data', method='POST')
+@managerapp.route('/app/post/data', methods=["POST"])
 def data():
-    json_request = request.json()
-    c = conn.cursor()
-    #insert into table ,if row already exist update the same row
-    c.execute('INSERT INTO {table} (INSTANCE_ID ,HOSTNAME ,INSTANCE_TYPE ,\
-                PUBLIC_IPV4 ,CPUUTILIZATION ,DISKUTILIZATION ,DISKSIZE  \
-                DISKAVAIL ,DISKUSED ,MEMORYUSAGE , MEMORYUTILIZATION \
-                MEMORYTOTAL, MEMORYAVAIL , LOG_TIME ) SELECT({instance_id}),({hostname}),({instance_type}),\
-                ({public_ipv4}),({cpuutilization}),({diskutilization}), ({disksize}),({diskavail}),({diskused}), \
-                ({memoryusage}),({memoryutilized}),({memorytotal}), ({memoryavail}), ({log_time}) \
-                WHERE NOT EXISTS(SELECT * FROM {table}  WHERE INSTANCE_ID=({instance_id}));\
-                '.format(instance_id = request_json["Metadata"]["instance-id"]),
-                hostname = request_json["Metadata"]["hostname"],
-                instance_type = request_json["Metadata"]["instance-type"],
-                public_ipv4= request_json["Metadata"]["public-ipv4"],
-                cpuutilization=request_json["CPUUtilization"]["CPUUtilization"]["Value"],
-                diskutilization= request_json["Diskutilization"]["Percentageused"],
-                disksize = request_json["Diskutilization"]["Totalsize"],
-                diskavail = request_json["Diskutilization"]["Avail"],
-                diskused = request_json["Diskutilization"]["Used"],
-                memoryusage = request_json["MemmoryUtilization"]["Mem_used"],
-                memoryutilized = request_json["MemmoryUtilization"]["MemoryUtilized"]["Value"],
-                memorytotal = request_json["MemmoryUtilization"]["Total_Mem"]
-                memoryavail = request_json["MemmoryUtilization"]["Available_Mem"]
-                log_time = request_json["Time"] ))
-    conn.commit()
-    conn.close()
+    if request.method == 'POST':
+        json_request = request.json()
+        
+        #Parsing value from request
+        instance_id = request_json["Metadata"]["instance-id"],
+        hostname = request_json["Metadata"]["hostname"],
+        instance_type = request_json["Metadata"]["instance-type"],
+        public_ipv4= request_json["Metadata"]["public-ipv4"],
+        cpuutilization=request_json["CPUUtilization"]["CPUUtilization"]["Value"],
+        diskutilization= request_json["Diskutilization"]["Percentageused"],
+        disksize = request_json["Diskutilization"]["Totalsize"],
+        diskavail = request_json["Diskutilization"]["Avail"],
+        diskused = request_json["Diskutilization"]["Used"],
+        memoryusage = request_json["MemmoryUtilization"]["Mem_used"],
+        memoryutilized = request_json["MemmoryUtilization"]["MemoryUtilized"]["Value"],
+        memorytotal = request_json["MemmoryUtilization"]["Total_Mem"],
+        memoryavail = request_json["MemmoryUtilization"]["Available_Mem"],
+        log_time = request_json["Time"] 
+        c = conn.cursor()
+
+        c.execute(f"INSERT INTO {table} (INSTANCE_ID ,HOSTNAME ,INSTANCE_TYPE ,\
+                    PUBLIC_IPV4 ,CPUUTILIZATION ,DISKUTILIZATION ,DISKSIZE  \
+                    DISKAVAIL ,DISKUSED ,MEMORYUSAGE , MEMORYUTILIZATION \
+                    MEMORYTOTAL, MEMORYAVAIL , LOG_TIME ) SELECT({instance_id}),\
+                    ({hostname}),({instance_type}),({public_ipv4}),({cpuutilization}),\
+                    ({diskutilization}), ({disksize}),({diskavail}),({diskused}), \
+                    ({memoryusage}),({memoryutilized}),({memorytotal}), ({memoryavail}),\
+                    ({log_time}) WHERE NOT EXISTS(SELECT * FROM {table}  \
+                    WHERE INSTANCE_ID=({instance_id}));")
+        conn.commit()
+        conn.close()
+        return jsonify({"Status":"Sucessfully recieved"})
+
+
 
 if __name__ == '__main__':
-    api.run(host='0.0.0.0', port=6000)
+    managerapp.run(host='0.0.0.0', port=6000)
